d977ca5b5b228fff8ec6aa078cdf7571fa0bfb45
Arnold-In-Tech <g16a7782@gmail.com>
2024-01-31 16:51:20 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad (event){
+    // function handleChange(){
+    //     console.log('Entering password...')
+    // }
+    // <input type="password" onChange={handleChange}/> 
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={() => console.log('Entering password...')}/> 
+        </div>
     )
 }
 
